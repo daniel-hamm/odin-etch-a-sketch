f396a5a37354078e6ed296e514c01e9ac867b243
Daniel Hamm <7501287+daniel-hamm@users.noreply.github.com>
2022-12-25 22:21:15 +0100

    Dynamically adjust the grid

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 // global variables
 let is_mouse_down = false;      // store the status if the mouse is clicked or not
+let square_size = 16;           // square size in px; 16, 32, 64
+let grid_max_width = 256;       // max width of the grid
+let grid_max_height = 256;      // max height of the grid
 
 // select the grid outline
 const grid_container = document.querySelector('.grid-outline');
+
+// ###### 2 is the border size and should be a variable ###### maybe no or way smaller border?
+// ###### adjust the border size depending on the number of squares ######
+// width and height dynamically styling of the grid container
+grid_container.style.width = `${grid_max_width + 2 * square_size}px`;   /* inner width => 16*16 = 256; plus grid border 1px each side => 2px * 16 = 32px */
+grid_container.style.height = `${grid_max_height + 2 * square_size}px`; /* inner height => 16*16 = 256; plus grid border 1px each side => 2px * 16 = 32px */
 
 // generate the square grid
 // we generate a 16x16 square with 16*16px of each square
 // as we use css flexbox wrap there only fit 16 squares in one row, because the width is respected
 // so we can generate all 256 squares at once
-for(let square_id = 0; square_id < 256; square_id ++) {
-    const square = document.createElement('div');         // set the square as div
-    square.classList.add('square');                       // add the same class to each square, so we can use them as group
-    square.setAttribute('id', `square-${square_id}`);     // give each square a unique id
-    grid_container.appendChild(square);                   // append the squares to the main grid
+for(let square_id = 0; square_id < (square_size * square_size); square_id ++) {
+    const square = document.createElement('div');                   // set the square as div
+    square.classList.add('square');                                 // add the same class to each square, so we can use them as group
+    square.setAttribute('id', `square-${square_id}`);               // give each square a unique id
+    square.style.width = `${grid_max_width / square_size}px`;       // 256px is the grid width without borders and should stay the same
+    square.style.height = `${grid_max_height / square_size}px`;     // so adjust the square width and height depending on the max grid values
+    grid_container.appendChild(square);                             // append the squares to the main grid
 }
 
 // select all squares as group 
